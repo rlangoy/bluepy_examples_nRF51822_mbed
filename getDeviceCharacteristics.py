d00665f7481e6433785234a22efa1e716dd52608
Rune Langøy <rune.langoy@gmail.com>
2016-04-15 07:19:35 +0000

    Added getDeviceCharacteristics.py

---
+++ getDeviceCharacteristics.py
@@ -0,0 +1,12 @@
+import sys
+from bluepy.btle import UUID, Peripheral
+
+if len(sys.argv) != 2:
+  print "Fatal, must pass device address:", sys.argv[0], "<device address="">"
+  quit()
+
+p = Peripheral(sys.argv[1],"random")
+
+chList = p.getCharacteristics()
+for ch in chList:
+   print ("Handle: 0x"+ format(ch.getHandle(),'02X')  +" "+str(ch.uuid) +" " + ch.propertiesToString())
