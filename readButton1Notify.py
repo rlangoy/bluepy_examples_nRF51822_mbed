1b6171192a1261b54114d19664a38bb24deb3a2c
Rune Langøy <rune.langoy@gmail.com>
2016-04-19 06:47:23 +0000

    Added a search to find the CCC

--- readButton1Notify.py
+++ readButton1Notify.py
@@ -19,8 +19,23 @@ if len(sys.argv) != 2:
 p = btle.Peripheral(sys.argv[1],"random")
 p.setDelegate( MyDelegate(p) )
 
-# Turn notifications on
-p.writeCharacteristic(0xf, struct.pack('<bb', 0x01, 0x00))
+#Get ButtonService
+ButtonService=p.getServiceByUUID(0xA000)
+# Get The Button-Characteristics
+ButtonC=ButtonService.getCharacteristics(0xA001)[0]
+#Get The handle tf the  Button-Characteristics
+hButtonC=ButtonC.getHandle()
+# Search and get Get The Button-Characteristics "property" (UUID-0x2902 CCC-Client Characteristic Configuration))
+#  wich is located in a handle in the range defined by the boundries of the ButtonService
+for desriptor in p.getDescriptors(hButtonC,0x00F):  # The handle range should be read from the services 
+   if (desriptor.uuid == 0x2902):                   #      but is not done due to a Bluez/BluePy bug :(     
+        print ("Button1 Client Characteristic Configuration found at handle 0x"+ format(desriptor.handle,"02X"))
+        hButtonCCC=desriptor.handle
+ 
+# Turn notifications on by setting bit0 in the CCC more info on:
+# https://developer.bluetooth.org/gatt/descriptors/Pages/DescriptorViewer.aspx?u=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml    
+p.writeCharacteristic(hButtonCCC, struct.pack('<bb', 0x01, 0x00))
+print "Notification is turned on for Button1"
 
 # Main loop --------
 while True:
