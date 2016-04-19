e42037890354460119d526d42f2697158d184bf9
Rune Langøy <rune.langoy@gmail.com>
2016-04-19 08:37:49 +0000

    Get Button1 characteristic from the Button Service

--- readButton1.py
+++ readButton1.py
@@ -4,16 +4,18 @@ import struct
 import time
 from bluepy.btle import UUID, Peripheral
 
-button_service_uuid = UUID(0xa001)
+button_service_uuid = UUID(0xA000)
+button_char_uuid    = UUID(0xA001)
 
 if len(sys.argv) != 2:
   print "Fatal, must pass device address:", sys.argv[0], "<device address="">"
   quit()
 
 p = Peripheral(sys.argv[1], "random")
+ButtonService=p.getServiceByUUID(button_service_uuid)
 
 try:
-    ch = p.getCharacteristics(uuid=button_service_uuid)[0]
+    ch = ButtonService.getCharacteristics(button_char_uuid)[0]
     if (ch.supportsRead()):
         while 1:
             val = binascii.b2a_hex(ch.read())
